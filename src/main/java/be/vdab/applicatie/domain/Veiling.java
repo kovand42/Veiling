61d3d8f3ed41370c9310be7858f3bd2e2a229489
kovand42 <kovand42@gmail.com>
2019-02-13 11:04:56 +0100

    Veiling tweede commit met VeilingTest method

--- src/main/java/be/vdab/applicatie/domain/Veiling.java
+++ src/main/java/be/vdab/applicatie/domain/Veiling.java
@@ -1,5 +1,10 @@
 package be.vdab.applicatie.domain;
 
 public class Veiling {
-
+	public void doeBod(int bedrag) {
+		throw new UnsupportedOperationException();
+	}
+	public int getHoogsteBod() {
+		throw new UnsupportedOperationException();
+	}
 }
--- src/test/java/be/vdab/applicatie/domain/VeilingTest.java
+++ src/test/java/be/vdab/applicatie/domain/VeilingTest.java
@@ -1,14 +1,18 @@
 package be.vdab.applicatie.domain;
 
 import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class VeilingTest {
-
+	private Veiling veiling;
+	@Before
+	public void before() {
+		veiling = new Veiling();
+	}
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void hetHoogsteBodVanEenNieuweVeilingIsNul() {
+		assertEquals(0, veiling.getHoogsteBod());
 	}
 
 }
